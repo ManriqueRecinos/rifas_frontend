670e68dcb422be46a65077079f6c56b40447f7f0
Manrique Recinos <manrique.recinos23@gmail.com>
2026-05-29 07:45:32 -0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 import RifaDetail from './pages/RifaDetail';
 import Login from './pages/Login';
 import Register from './pages/Register';
